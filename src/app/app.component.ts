962ad9a7e4efa475ec0c93947e73bd7a9f28308c
viral champanery <viral@arbox.com>
2020-11-20 21:20:49 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-dreamp-app';
+export class AppComponent implements OnInit  {
+  title = 'my-dreamp-app=test';
+  servers= [
+            {'name':'test1','phone':'123'},
+            {'name':'test2','phone':'345'},
+          ];
+  collection = [];
+  showmsg = false;
+  p = 1;
+  constructor(private http: HttpClient){
+    for(let i=1;i<=100;i++){
+      let Obj = {'name': `Employee Name ${i}`,'code': `EMP00 ${i}`}
+      this.collection.push(Obj);
+    }
+  }
+  ngOnInit() {
+    
+    this.http.get<any>('http://62.75.137.184:8000/api/products').subscribe(data => {
+      // this.totalAngularPackages = data.total;
+      console.dir(data.data);
+      this.servers = data.data;
+    })       
+  }
+
+  //remove
+  remove(id:any){
+    console.dir("call remove : "+id);
+    this.showmsg = false;
+  }
+  edit(id:any){
+    console.dir("call edit : "+id);
+    this.showmsg = true;
+  }
+  //save
+  save(){
+    console.dir("call save");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPaginationModule,
+       HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
